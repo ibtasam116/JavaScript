01a9579c3b77cb5bc1391a4ebb3c05b4b2cb3909
ibtasam116 <ibtasam116@gmail.com>
2023-08-09 10:08:17 +0500

    JavaScript

---
+++ 02.Variables in JS/index.js
@@ -0,0 +1,9 @@
+// Practice for variable
+// = means assignment operator used for assigning a variable value
+// keyword or reserved word,
+var age = 20
+var height = 5.5
+
+var name = "Ibtasam Jutt"
+
+
