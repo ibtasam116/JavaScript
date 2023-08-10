9298d00cf075450c33d6d6b03b2473fb13a4a788
ibtasam116 <ibtasam116@gmail.com>
2023-08-10 11:08:47 +0500

    Types

--- 03.Variables in JS/index.js
+++ 03.Variables in JS/index.js
@@ -1,4 +1,25 @@
+
+// rules for defining variables
+// varibale can't start  from numbers / special
+// variable is case senstive
+// always use camelCase while defining variable name 
+
 var Name = "Ibtasam";
 
 var name = "ibtasam";
+
+var firstNameForUser = "Muhammad";  // camelCase Variable example    camelCase mean first letter in variable always small and rest of letter is capital.
+var FirstNameForUser = "Muhammad"; // Pascal Case variable example   PascalCase mean first letter in variable always capital.
+
+// Types in programing languages
+
+// "" or '' or `` representing to a string
+var string = "123" // 123 is not a number this is string
+var num1 = 123
+console.log("String", typeof (string));
+console.log("num1", typeof (num1));
+
+var flag = false;
+console.log("Flag => ", typeof (flag), flag);
+
 
