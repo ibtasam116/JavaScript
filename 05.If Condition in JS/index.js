224fe23b902369f95e52b76d5f25ce49ea154ac9
ibtasam116 <ibtasam116@gmail.com>
2023-08-12 12:56:35 +0500

    Nice:

---
+++ 05.If Condition in JS/index.js
@@ -0,0 +1,36 @@
+// Topic => What is if conditions
+// Need to understand the flow of lines excution
+// Flow will be top to bottom - default structure
+console.log("Line4");
+console.log("Line5");
+console.log("Line6");
+console.log("Line7");
+console.log("Line8");
+console.log("Line9");
+console.log("Line10");
+
+
+// if structure true condition 
+console.log("<====== If structure with true condition start ======>");
+console.log("Line15");
+console.log("Line16");
+if (true) {
+    console.log("Line18");
+    console.log("Line19");
+    console.log("Line20");
+}
+console.log("Line22");
+console.log("Line23");
+
+// if structure false condition 
+console.log("<====== If structure with false condition start ======>");
+console.log("Line27");
+console.log("Line28");
+if (false) {
+    console.log("Line30");
+    console.log("Line31");
+    console.log("Line32");
+}
+console.log("Line34");
+console.log("Line35");
+
