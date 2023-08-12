b27031602ee8abff5b784b65614e2640a45123b2
ibtasam116 <ibtasam116@gmail.com>
2023-08-12 11:11:09 +0500

    Good Work

--- 04.Camparison Operators in JS/index.js
+++ 04.Camparison Operators in JS/index.js
@@ -1,2 +1,49 @@
 // < > == >= <=
 
+console.log("Check greater than");
+var num1 = 5
+var num2 = 7
+
+var result = num1 > num2
+var result2 = num2 > num1
+
+console.log("num1 5 > num2 7 ==> ", result);
+console.log("num2 7 > num1 5 ==> ", result2);
+
+console.log("Check less than");
+var result = num1 < num2
+var result2 = num2 < num1
+
+console.log("num1 5 > num2 7 ==> ", result);
+console.log("num2 7 > num1 5 ==> ", result2);
+
+
+// = is assignment operator
+// == is equal to operator
+
+console.log("Check equal to");
+var num3 = 5
+var num4 = 5
+
+var result3 = num3 == num4
+var result4 = num3 != num4
+
+console.log("num3 5 == num4 5 ==> ", result3);
+console.log("num3 5 != num4 5 ==> ", result4);
+
+
+console.log("Check greater than equal to");
+var num5 = 7
+var num6 = 7
+
+var result5 = num5 >= num6
+
+console.log("num5 7 >= num6 7", result5);
+
+console.log("Check less than equal to");
+var num7 = 2
+var num8 = 6
+
+var result6 = num7 <= num8
+
+console.log("num7 2 <= num8 6", result6);
