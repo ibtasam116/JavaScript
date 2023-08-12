a6ad913ccc9d22621a1d6a66fc8baa00c69e9326
ibtasam116 <ibtasam116@gmail.com>
2023-08-12 10:19:06 +0500

    Good

--- 03.Problem Solving in JS/index.js
+++ 03.Problem Solving in JS/index.js
@@ -3,3 +3,25 @@
 // Why it's importantin software development?
 
 
+
+
+// write a program to convert 5c temp into f?    Formula =>  (0°C × 9/5) + 32 = 32°F
+
+var temInC = 13;
+var temInF = (temInC * 9 / 5) + 32
+// temInF = temInF.toString();
+// temInF = parseInt(temInF);
+// temInF = Number(temInF);
+console.log("temp in f =>", temInF, typeof (temInF));
+
+
+// D Mas Rule
+
+console.log(2*5+2/5);
+
+// Pref
+// * / Same level
+// + - Same Level
+
+
+
