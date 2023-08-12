5fba2664f85a90071351f0d9922e78e442e5976b
ibtasam116 <ibtasam116@gmail.com>
2023-08-12 12:38:32 +0500

    Good Work

--- 04.Camparison Operators in JS/index.js
+++ 04.Camparison Operators in JS/index.js
@@ -47,3 +47,36 @@ var num8 = 6
 var result6 = num7 <= num8
 
 console.log("num7 2 <= num8 6", result6);
+
+
+// === equal to operator with data types
+console.log("Check data types are equal");
+var num9 = "9"
+var num10 = 9
+var result7 = num9 === num10
+console.log('num9 "9" === num10 10', result7);
+
+
+// AND operator &&
+// T && T  => T => 2 == 2 && 5 == 5 => T
+// T && F  => F => 2 == 2 && 7 == 5 => F
+// F && T  => F => 2 == 1 && 5 == 5 => F
+// F && F  => F => 1 == 2 && 9 == 5 => F
+
+// OR operator ||
+// T || T  => T => 2 == 2 || 5 == 5 => T
+// T || F  => T => 2 == 2 || 5 == 7 => T
+// F || T  => T => 2 == 1 || 5 == 5 => T
+// F || F  => F => 2 == 9 || 3 == 5 => F
+
+console.log("Check AND Operator");
+var x = 9
+var y = 10
+var n = 4
+var m = 8
+
+var flag = x <= y && n > m
+console.log("x 9 <= y 10 && n 4 > m 8", flag);
+
+
+
